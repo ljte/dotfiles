827e918289b0e122f31b84c0d514cd8d39ace27b
Dzmitry Orgish <ljte823@gmail.com>
2023-03-22 15:00:52 +0300

    change shortcuts for telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -280,12 +280,12 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>pf', require('telescope.builtin').find_files, { desc = 'Search [P]roject [F]iles' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = 'Search [P]roject [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>cw', require('telescope.builtin').grep_string, { desc = 'Search [C]urrent [W]ord' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = 'Search [C]urrent [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>rf', require('telescope.builtin').git_files, { desc = 'Search [R]epository [F]iles' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').git_files, { desc = 'Search [R]epository [F]iles' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
